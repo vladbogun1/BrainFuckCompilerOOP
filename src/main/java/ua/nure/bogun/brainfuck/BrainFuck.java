5d9b699851f4a11b36411ac5c4afe8d4d55cc3ac
vladbogun1 <vladbogun@gmail.com>
2019-12-12 23:07:59 +0200

    Add class "LoopCommand". "LoopCommand" represents the functionality of the program. Update some classes to work with "LoopOperator". Modify some in encapsulation.

--- src/main/java/ua/nure/bogun/brainfuck/BrainFuck.java
+++ src/main/java/ua/nure/bogun/brainfuck/BrainFuck.java
@@ -1,29 +1,40 @@
 package ua.nure.bogun.brainfuck;
+import java.util.List;
 
 class BrainFuck {
     private Array memory;
 
-    BrainFuck(){
+    BrainFuck() {
         this.memory = new Array();
     }
-    void next(){
-        memory.setPointer(memory.getPointer()+1);
+
+    void next() {
+        memory.setPointer(memory.getPointer() + 1);
     }
-    void prev(){
-        memory.setPointer(memory.getPointer()-1);
+
+    void prev() {
+        memory.setPointer(memory.getPointer() - 1);
     }
-    void increment(){
+
+    void increment() {
         memory.arr[memory.getPointer()]++;
     }
-    void decrement(){
-        if(memory.arr[memory.getPointer()]>0){
+
+    void decrement() {
+        if (memory.arr[memory.getPointer()] > 0) {
             memory.arr[memory.getPointer()]--;
         }
     }
-    void  output(){
-        System.out.print((char)memory.arr[memory.getPointer()]);
-    }
-    void loop(){
 
+    void output() {
+        System.out.print((char) memory.arr[memory.getPointer()]);
+    }
+
+    void loop(List<Command> commands) {
+        while (memory.arr[memory.getPointer()] > 0) {
+            for (Command command : commands) {
+                command.execute();
+            }
+        }
     }
 }
--- src/main/java/ua/nure/bogun/brainfuck/Demo.java
+++ src/main/java/ua/nure/bogun/brainfuck/Demo.java
@@ -3,7 +3,8 @@ package ua.nure.bogun.brainfuck;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Representing BrainFuck Compiler:");
-        Compiler compiler = new Compiler(">+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.>++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.");
+        BrainFuck brainFuck = new BrainFuck();
+        Compiler compiler = new Compiler("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.", brainFuck);
         compiler.execute();
     }
 }
