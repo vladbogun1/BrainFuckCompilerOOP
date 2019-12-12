dc13cb55cb8393594f7e668c72aa202bf07580d9
vladbogun1 <vladbogun@gmail.com>
2019-12-12 16:26:31 +0200

    Update class "Demo". "Demo" represents the functionality of the program.

--- src/main/java/ua/nure/bogun/brainfuck/Demo.java
+++ src/main/java/ua/nure/bogun/brainfuck/Demo.java
@@ -3,5 +3,7 @@ package ua.nure.bogun.brainfuck;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Representing BrainFuck Compiler:");
+        Compiler compiler = new Compiler(">+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.>++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.");
+        compiler.execute();
     }
 }
