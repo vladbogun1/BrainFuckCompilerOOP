c62500753c7caba1b3e9a67e57391cf862e20e8b
vladbogun1 <vladbogun@gmail.com>
2019-12-15 09:03:37 +0200

    Create runnable .bat file to run program.

--- src/main/java/ua/nure/bogun/brainfuck/Demo.java
+++ src/main/java/ua/nure/bogun/brainfuck/Demo.java
@@ -1,10 +1,17 @@
 package ua.nure.bogun.brainfuck;
 
 public class Demo {
+
     public static void main(String[] args) {
-        System.out.println("Representing BrainFuck Compiler:");
+        String command = (args.length > 0 && args[0] != null) ? args[0] :
+                "++++++++++[>+>+++>+++++++>++++++++++<<<<-]" +
+                        ">>>++++++++++++++.>+++++++++++++++++++++" +
+                        ".---------.-----------.<<++.>>--.++++++++++++" +
+                        ".--..------------.+++++++++++++.----------.<<" +
+                        ".>>---.++++++++++++++++++.<<.>+++++++++++++.>-" +
+                        ".<++++++.>+++.<++++++.--------.>-------.++++++.";
         BrainFuck brainFuck = new BrainFuck();
-        Compiler compiler = new Compiler("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.", brainFuck);
+        Compiler compiler = new Compiler(command, brainFuck);
         compiler.execute();
     }
 }
